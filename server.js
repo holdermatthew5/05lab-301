c71c1cb9128cd05c95c28a583330bcaf8a583d7f
Matthew Holder <holdermatthew5@gmail.com>
2020-11-12 13:27:18 -0800

    update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors);
+app.use(cors());
 app.use(express.static('./public'));
 
 app.get('/hello', (request, response) => {
